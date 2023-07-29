272f34be7ab45e9659130a7afafc12c147584956
github-action-benchmark <github@users.noreply.github.com>
2023-07-29 22:56:39 +0000

    add Benchmark (pytest) benchmark result for 8ed61e9d1dca18760db97b53f90b416917a429a7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690670039978,
+  "lastUpdate": 1690671399101,
   "repoUrl": "https://github.com/AzureAD/microsoft-authentication-library-for-python",
   "entries": {
     "Benchmark": [
@@ -80,6 +80,86 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00001797239158016403",
             "extra": "mean: 146.46402795809072 usec\nrounds: 5079"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rayluo@microsoft.com",
+            "name": "Ray Luo",
+            "username": "rayluo"
+          },
+          "committer": {
+            "email": "rayluo@microsoft.com",
+            "name": "Ray Luo",
+            "username": "rayluo"
+          },
+          "distinct": true,
+          "id": "8ed61e9d1dca18760db97b53f90b416917a429a7",
+          "message": "Do not run benchmark in matrix",
+          "timestamp": "2023-07-29T15:53:29-07:00",
+          "tree_id": "da99ba69ebda5b9005e994f69a2aaf7192bf4cca",
+          "url": "https://github.com/AzureAD/microsoft-authentication-library-for-python/commit/8ed61e9d1dca18760db97b53f90b416917a429a7"
+        },
+        "date": 1690671398662,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmark.py::test_cca_1_tenant_with_1_token_per_tenant_and_cache_hit",
+            "value": 34135.62250920954,
+            "unit": "iter/sec",
+            "range": "stddev: 9.572619386751023e-7",
+            "extra": "mean: 29.294910316347895 usec\nrounds: 10236"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_cca_1_tenant_with_10_tokens_per_tenant_and_cache_hit",
+            "value": 23151.003956619144,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000010377350230142023",
+            "extra": "mean: 43.194671033438624 usec\nrounds: 20534"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_cca_1_tenant_with_1000_tokens_per_tenant_and_cache_hit",
+            "value": 668.3612124629099,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000010152209091874527",
+            "extra": "mean: 1.4961969386508858 msec\nrounds: 652"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_cca_many_tenants_with_10_tokens_per_tenant_and_cache_hit",
+            "value": 23062.705399971182,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000001500287878974986",
+            "extra": "mean: 43.36004742970222 usec\nrounds: 20367"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_cca_1_tenant_with_1_token_per_tenant_and_cache_miss",
+            "value": 8206.922332122947,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000018413488113040616",
+            "extra": "mean: 121.84835673244665 usec\nrounds: 6610"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_cca_1_tenant_with_10_tokens_per_tenant_and_cache_miss",
+            "value": 7352.232496705283,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000017922074598120608",
+            "extra": "mean: 136.01310900439083 usec\nrounds: 6330"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_cca_1_tenant_with_1000_tokens_per_tenant_and_cache_miss",
+            "value": 619.5530113995497,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002439858844106065",
+            "extra": "mean: 1.6140668862879597 msec\nrounds: 598"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_cca_many_tenants_with_10_tokens_per_tenant_and_cache_miss",
+            "value": 7239.102061034979,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001860609111086352",
+            "extra": "mean: 138.13867957223266 usec\nrounds: 6173"
           }
         ]
       }
